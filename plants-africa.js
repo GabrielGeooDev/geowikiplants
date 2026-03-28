6d764f26289dedcec3be354d86a128887a8dad61
GabrielGeooDev <gabrielgeoguessrcontact@gmail.com>
2026-03-28 17:10:18 +0100

    Update plants-africa.js

--- plants-africa.js
+++ plants-africa.js
@@ -14,6 +14,36 @@
 
 const PLANTS_AFRICA = [
 
+{
+    id: 'borassus-akeassii',           // identifiant unique (pas d'espaces, pas d'accents)
+    sci: 'Borassus akeassii',          // nom scientifique
+    common: 'West African Palmyra Palm',      // nom commun en anglais
+    continent: 'africa',            // africa | asia | europe | north-america | south-america | oceania
+    category: 'palms',                     // palms | trees | euphorbias | succulents | crops | others | bromeliads | cacti | bamboos | hedges | flowers | ferns
+    diff: 1,                               // difficulté d'identification : 1 (facile) | 2 | 3 (difficile)
+    freq: 3,                               // fréquence d'apparition   : 1 (rare)    | 2 | 3 (commun)
+    desc: '<em>Texte en gras</em> à faire plus tard.',
+    tips: [
+      'test',
+      'test',
+      'test'
+    ],
+    similar: ['Borassus aethiopum'],  // noms scientifiques des espèces similaires
+    family: 'Arecaceae',
+    genus: 'Borassus',
+    countries: ['Senegal'],                 // liste des pays (noms en anglais)
+    inat: 'https://www.inaturalist.org/taxa/364450-Borassus-akeassii',
+    wiki: 'https://en.wikipedia.org/wiki/Borassus_akeassii',
+    photos: ['https://upload.wikimedia.org/wikipedia/commons/thumb/8/8b/P1080424-_Borassus_akeassii_-_Casamance.jpg/960px-P1080424-_Borassus_akeassii_-_Casamance.jpg'
+      // URLs directes vers tes images (imgur, cloudinary, etc.)
+      // 'https://i.imgur.com/XXXXXXX.jpg',
+    ],
+    maps: [
+      // { label: 'Brazil', img: 'https://i.imgur.com/XXXXXXX.png' },
+      // { label: 'Ghana',  img: 'https://i.imgur.com/YYYYYYY.png' },
+    ]
+  },
+  
   // ── EXEMPLE ─────────────────────────────────────────────────
   // {
   //   id: 'borassus-aethiopum',
